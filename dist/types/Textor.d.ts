180f7c45f691acb40976e7979437aa09e7fdf920
Evan <evansmalley2@gmail.com>
2026-05-05 15:22:51 -0400

    dist

--- dist/types/Textor.d.ts
+++ dist/types/Textor.d.ts
@@ -1,5 +1,9 @@
 export declare class Textor {
     static levenshtein(a: string, b: string): number;
     static toSentenceCase(str: string): string;
+    /**
+     * Generates pseudo-Latin Lorem Ipsum placeholder text.
+     */
+    static generateLoremIpsum(paragraphs?: number, sentencesPerParagraph?: number, startWithLorem?: boolean): string;
 }
 //# sourceMappingURL=Textor.d.ts.map
