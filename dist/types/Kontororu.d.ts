fe27ea308f48a4b927cf20d30fed94298835c80b
Evan <evansmalley2@gmail.com>
2026-05-05 23:03:07 -0400

    change license

--- dist/types/Kontororu.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Kontororu extends EventTarget {
-    constructor();
-    private listeners;
-    addEventListener(type: string, listener: (...args: unknown[]) => void): void;
-    removeEventListener(type: string, listener: (...args: unknown[]) => void): void;
-    getListeners(type: string): ((...args: unknown[]) => void)[];
-}
-//# sourceMappingURL=Kontororu.d.ts.map
--- dist/types/Textor.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Textor {
-    static levenshtein(a: string, b: string): number;
-    static toSentenceCase(str: string): string;
-    /**
-     * Generates pseudo-Latin Lorem Ipsum placeholder text.
-     */
-    static generateLoremIpsum(paragraphs?: number, sentencesPerParagraph?: number, startWithLorem?: boolean): string;
-}
-//# sourceMappingURL=Textor.d.ts.map
